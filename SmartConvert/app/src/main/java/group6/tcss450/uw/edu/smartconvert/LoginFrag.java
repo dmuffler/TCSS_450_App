ef88ce948ad3fd5830307d0ac458dd9559842f5a
U-DONALD\donal <donald.muffler@live.com>
2017-10-23 15:38:52 -0700

    Added the start and login screen. Connected the two.

--- SmartConvert/app/src/main/java/group6/tcss450/uw/edu/smartconvert/LoginFrag.java
+++ SmartConvert/app/src/main/java/group6/tcss450/uw/edu/smartconvert/LoginFrag.java
@@ -8,64 +8,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link StartFrag.StartFragInteractionListener} interface
+ * {@link LoginFrag.LoginFragmentInteractionListener} interface
  * to handle interaction events.
  */
-public class StartFrag extends Fragment implements View.OnClickListener {
+public class LoginFrag extends Fragment implements View.OnClickListener {
 
-    private StartFragInteractionListener mListener;
+    private LoginFragmentInteractionListener mListener;
 
-    public StartFrag() {
+    public LoginFrag() {
         // Required empty public constructor
     }
 
+    EditText userNameTextField;
+    EditText userPassTextField;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_start, container, false);
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
 
-        Button b = (Button) v.findViewById(R.id.loginButton);
+        Button b = (Button) v.findViewById(R.id.submitButton);
         b.setOnClickListener(this);
-        b = (Button) v.findViewById(R.id.skipButton);
-        b.setOnClickListener(this);
-        b = (Button) v.findViewById(R.id.registerButton);
-        b.setOnClickListener(this);
+
+        userNameTextField = (EditText) v.findViewById(R.id.usernameField);
+        userPassTextField = (EditText) v.findViewById(R.id.passwordField);
 
         return v;
     }
 
     @Override
-    public void onClick(View view) {
-        if (mListener != null) {
-            switch (view.getId()) {
-                case R.id.loginButton:
-                    String loginFrag = "Login";
-                    mListener.startFragInteraction(loginFrag);
-                    break;
-                case R.id.skipButton:
-                    String skipFrag = "Skip";
-                    mListener.startFragInteraction(skipFrag);
-                    break;
-                case R.id.registerButton:
-                    String registerFrag = "Register";
-                    mListener.startFragInteraction(registerFrag);
-                    break;
-            }
-        }
-    }
-
-    @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof StartFragInteractionListener) {
-            mListener = (StartFragInteractionListener) context;
+        if (context instanceof LoginFragmentInteractionListener) {
+            mListener = (LoginFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -78,6 +60,16 @@ public class StartFrag extends Fragment implements View.OnClickListener {
         mListener = null;
     }
 
+    @Override
+    public void onClick(View view) {
+        if (mListener != null) {
+            if (view.getId() == R.id.submitButton) {
+                String homeFrag = "Home";
+                mListener.loginFragmentInteraction(homeFrag);
+            }
+        }
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -88,8 +80,8 @@ public class StartFrag extends Fragment implements View.OnClickListener {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface StartFragInteractionListener {
+    public interface LoginFragmentInteractionListener {
         // TODO: Update argument type and name
-        void startFragInteraction(String fragString);
+        void loginFragmentInteraction(String fragString);
     }
 }
